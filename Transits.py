748c229086260de57f5ffc0de671e7d6e6e32587
rsmith1017 <63197254+rsmith1017@users.noreply.github.com>
2020-04-12 15:58:57 -0400

    Add files via upload

---
+++ Transits.py
@@ -0,0 +1,14 @@
+# Transit PYthon
+
+
+print ("Check schedules and Dates.")
+text_file = open("Transits.txt", "r")
+text_file.close()
+
+print ("Today's Schedule")
+text_file = open("Transits.txt", "r")
+print (text_file.read())
+text_file.close()
+
+              
+                 
